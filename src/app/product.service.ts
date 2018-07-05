70718a23a84d69f86b50ab5c87cf7f8967ada336
mh453Uol <hussainmajid95@gmail.com>
2018-07-05 11:19:09 +0100

    fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,10 +1,10 @@
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  private _albumUrl = './assets/album.json';
+  private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
